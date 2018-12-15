54f27d9990266cd7e5f22562acf2522717ef5fa0
tsan <ahtsans@gmail.com>
2018-12-15 15:20:50 -0800

    Final modification

--- garage/envs/wrappers/stack_frames.py
+++ garage/envs/wrappers/stack_frames.py
@@ -32,7 +32,7 @@ class StackFrames(gym.Wrapper):
 
         super().__init__(env)
 
-        self.n_frames = n_frames
+        self._n_frames = n_frames
         self._frames = deque(maxlen=n_frames)
 
         new_obs_space_shape = env.observation_space.shape + (n_frames, )
@@ -57,7 +57,7 @@ class StackFrames(gym.Wrapper):
         """gym.Env reset function."""
         observation = self.env.reset()
         self._frames.clear()
-        for i in range(self.n_frames):
+        for i in range(self._n_frames):
             self._frames.append(observation)
 
         return self._stack_frames()
